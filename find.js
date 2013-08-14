10ac87a0978c8a55184d8ffbf45c4b81b8c1cc2d
Fabian Jakobs <fabian@c9.io>
2013-08-14 10:31:01 +0000

    initial commit

---
+++ find.js
@@ -0,0 +1,172 @@
+define(function(require, exports, module) {
+    main.consumes = ["plugin", "fs", "finder", "util"];
+    main.provides = ["find"];
+    return main;
+
+    function main(options, imports, register) {
+        var fs       = imports.fs;
+        var Plugin   = imports.plugin;
+        var finder   = imports.finder;
+        var util     = imports.util;
+        
+        /***** Initialization *****/
+        
+        var plugin = new Plugin("Ajax.org", main.consumes);
+        // var emit   = plugin.getEmitter();
+        
+        plugin.__defineGetter__("basePath", function(){ return basePath; });
+        
+        var basePath   = options.basePath;
+        var retrieving = false;
+        var queue      = [];
+        var cached, cacheTime;
+        
+        // var loaded = false;
+        // function load(){
+        //     if (loaded) return false;
+        //     loaded = true;
+        // }
+        
+        /***** Methods *****/
+        
+        function getFileList(options, callback){
+            if (cached && !options.nocache 
+              && new Date() - cacheTime < 60 * 60 * 1000)
+                return callback(null, cached);
+    
+            queue.push([options, callback]);
+            
+            if (retrieving)
+                return;
+            
+            if (!options.base)
+                options.base = basePath;
+    
+            cached     = "";
+            retrieving = true;
+            
+            finder.list(options, function(err, stream) {
+                if (!err) {
+                    cacheTime  = new Date();
+                }
+                retrieving = false;
+
+                var needsBuffer = [];
+                queue.forEach(function(iter){
+                    if (err || !iter[0].buffer)
+                        iter[1](err, stream) 
+                    else
+                        needsBuffer.push(iter[1]);
+                });
+                queue = [];
+                
+                if (err || !needsBuffer) return;
+                
+                cached = "";
+                stream.on("data", function(lines){
+                    cached += lines;
+                });
+                stream.on("end", function(){
+                    if (options.base && options.base != "/") {
+                        var rgx = new RegExp(util.escapeRegExp(options.base), "g");
+                        cached  = cached.replace(rgx, "").replace(/\\/g, "/")
+                    }
+                    
+                    needsBuffer.forEach(function(cb){
+                        cb(null, cached);
+                    });
+                });
+            });
+        }
+        
+        function findFiles(options, callback){
+            if (!options.base)
+                options.base = basePath;
+            
+            finder.find(options, function(err, stream){
+                if (err || !options.buffer)
+                    return callback(err, stream);
+                
+                var buffer = "";
+                stream.on("data", function(lines){
+                    buffer += lines;
+                });
+                stream.on("end", function(){
+                    if (options.base && options.base != "/") {
+                        var rgx = new RegExp(util.escapeRegExp(options.base), "g");
+                        buffer = buffer.replace(rgx, "").replace(/\\/g, "/");
+                    }
+                    callback(null, buffer);
+                });
+            });
+        }
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function(){
+            // load();
+        });
+        plugin.on("enable", function(){
+            
+        });
+        plugin.on("disable", function(){
+            
+        });
+        plugin.on("unload", function(){
+            loaded = false;
+        });
+        
+        /***** Register and define API *****/
+        
+        /**
+         * Finds or lists files and/or lines based on their filename or contents
+         **/
+        plugin.freezePublicAPI({
+            /**
+             * Retrieves a list of files and lines that match a string or pattern
+             * This method tries to do intelligent caching by hooking into the
+             * fs and watcher.
+             * @param options {Object}
+             *   object:
+             *   path           {String}  the path to search in (displayed in the results). Defaults to "".
+             *   base           {String}  the base path to search in (is not displayed in the results when buffered). Defaults to the fs root.
+             *   query          {String}  the text or regexp to match the file contents with
+             *   casesensitive  {Boolean} whether to match on case or not. Default is false;
+             *   wholeword      {Boolean} whether to match the `pattern` as a whole word.
+             *   hidden         {String}  include files starting with a dott. Defaults to false.
+             *   regexp         {String}  whether the `pattern` is a regular expression.
+             *   pattern        {String}  specify what files/dirs to include 
+             *      and exclude. Prefix the words with minus '-' to exclude.
+             *   replaceAll     {Boolean} whether to replace the found matches
+             *   replacement    {String}  the string to replace the found matches with
+             *   buffer         {Boolean} whether to buffer the request. This changes 
+             *      what is returned in the callback to a string instead of a stream.
+             * @param callback(err, results) {Function} called when the results come in
+             *   err     {Error}
+             *   results {Stream|String}
+             */
+            findFiles : findFiles,
+            
+            /**
+             * Retrieves a list of files under a path
+             * @param options {Object}
+             *   object:
+             *   path     {String}  the path to search in (displayed in the results). Defaults to "".
+             *   base     {String}  the base path to search in (is not displayed in the results when buffered). Defaults to the fs root.
+             *   hidden   {Boolean} include files starting with a dott. Defaults to false.
+             *   maxdepth {Number}  maximum amount of parents a file can have.
+             *   nocache  {Boolean} ignore the cache
+             *   buffer   {Boolean} whether to buffer the request. This changes 
+             *      what is returned in the callback to a string instead of a stream.
+             * @param callback(err, results) {Function} called when the results come in
+             *   err     {Error}
+             *   results {Stream|String}
+             */
+            getFileList : getFileList
+        });
+        
+        register(null, {
+            find: plugin
+        });
+    }
+});
---
+++ find.nak.js
@@ -0,0 +1,307 @@
+/**
+ * File Finder module for the Cloud9 IDE that uses nak
+ *
+ * @copyright 2012, Ajax.org B.V.
+ * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
+ */
+define(function(require, exports, module) {
+    main.consumes = ["plugin", "preferences", "ext", "fs", "proc", "nodeapi"];
+    main.provides = ["finder"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin   = imports.plugin;
+        var prefs    = imports.preferences;
+        var proc     = imports.proc;
+        var fs       = imports.fs;
+        
+        var PATH     = imports.nodeapi.path;
+        
+        /***** Initialization *****/
+        
+        var plugin = new Plugin("Ajax.org", main.consumes);
+        // var emit   = plugin.getEmitter();
+        
+        const MAIN_IGNORE = "/.c9/.nakignore";
+        const TEMPLATE    = require("text!./nakignore-template") 
+            + "\n" + (options.ignore || "");
+        const IGNORE      = options.ignore;
+        
+        const NAK = options.nak || "~/.c9/node_modules/nak/bin/nak";
+        
+        function install(callback, progress){
+            // Check if nak is already installed
+            fs.exists("~/.c9/node_modules/nak/bin/nak", function(exists) {
+                if (exists)
+                    return callback();
+            
+                progress("Installing nak");
+                        
+                // Create node_modules
+                fs.mkdirP("~/.c9/node_modules", function(){
+                    
+                    // Install nak
+                    proc.spawn("npm", {
+                        args : ["install", "nak"],
+                        cwd  : "~/.c9"
+                    }, function(err, process){
+                        if (err) return callback(err);
+                        
+                        var success = false;
+                        process.stderr.on("data", function(chunk){
+                            progress(chunk, true, true);
+                        });
+                        
+                        process.stdout.on("data", function(chunk){
+                            success = success || chunk.match(/nak@[\d\.]+/);
+                            progress(chunk, true);
+                        });
+                        
+                        process.stdout.on("end", function(){
+                            if (!success)
+                                return callback(new Error("Could not install nak"));
+                            
+                            progress("Setting up nak ignore file");
+                            
+                            // Set up nakignore
+                            fs.exists(MAIN_IGNORE, function(exists){
+                                if (!exists) {
+                                    fs.mkdir("/.c9", function(err){
+                                        if (err) return callback(err);
+                                        
+                                        fs.writeFile(MAIN_IGNORE, TEMPLATE, function(err){
+                                            callback(err);
+                                        });
+                                    });
+                                }
+                                else
+                                    callback();
+                            });
+                        });
+                    });
+                });
+            });
+        }
+        
+        var loaded = false;
+        function load(callback){
+            if (loaded) return;
+            loaded = true;
+            
+            prefs.add({
+               "General" : {
+                   position : 100,
+                   "Find in Files" : {
+                       position : 30,
+                       "Ignore these files" : {
+                           name      : "txtPref",
+                           type      : "textarea",
+                           width     : 150,
+                           height    : 130,
+                           rowheight : 155,
+                           position  : 1000
+                       }
+                   }
+               }
+            }, plugin);
+            
+            prefs.on("draw", function(){
+                var ta = plugin.getElement("txtPref").lastChild;
+                
+                ta.on("afterchange", function(e){
+                    fs.writeFile(MAIN_IGNORE, "utf8", e.value, function(){});
+                });
+                
+                fs.readFile(MAIN_IGNORE, function(err, data){
+                    if (err)
+                        data = TEMPLATE;
+                    
+                    ta.setValue(data);
+                });
+            }, plugin);
+        }
+        
+        /***** Methods *****/
+        
+        function assembleFilelistCommand(options) {
+            var args;
+    
+            args = ["-l"]; //, "-a", MAIN_IGNORE]; // -l = filenames only
+            
+            if (options.hidden)
+                args.push("-H");
+                
+            if (options.maxdepth)
+                args.push("-m", options.maxdepth);
+                
+            args.push(options.path);
+    
+            return args;
+        }
+    
+        function assembleSearchCommand(options) {
+            var args, query = options.query;
+    
+            if (!query)
+                return;
+    
+            args = []; //"-a", MAIN_IGNORE];
+    
+            if (!options.casesensitive)
+                args.push("-i");
+    
+            if (options.wholeword)
+                args.push("-w");
+    
+            if (options.hidden)
+                args.push("-H");
+                
+            if (!options.regexp)
+                args.push("-q");
+    
+            var includes = [], excludes = [];
+    
+            if (options.pattern) {
+                // strip whitespace, grab out exclusions
+                options.pattern.split(",").forEach(function (p) {
+                    // strip whitespace
+                    p = p.replace(/\s*/g, "");
+    
+                    if (/^\-/.test(p))
+                        excludes.push(p.substring(1));
+                    else
+                        includes.push(p);
+                });
+            }
+                
+            if (IGNORE)
+                excludes.push(IGNORE);
+
+            // wildcard handling will be done in nak
+            if (includes.length)
+                args.push("-G", includes.join(", "));
+
+            if (excludes.length)
+                args.push("--ignore", excludes.join(", "));
+    
+            args.push(query);
+    
+            if (options.replaceAll && options.replacement)
+                args.push(options.replacement);
+            
+            args.push(options.path);
+            
+            return args;
+        }
+        
+        function list(options, callback){
+            options.uri  = options.path || "";
+            options.path = PATH.join((options.base || ""), (options.path || ""));
+            
+            if (!options.path)
+                return callback(new Error("Invalid Path"));
+            
+            var args = assembleFilelistCommand(options);
+            if (!args)
+                return callback(new Error("Invalid Arguments"));
+            
+            execute(args, function(err, results){
+                callback(err, results && results.stream);
+            });
+        }
+        
+        function find(options, callback){
+            options.uri  = options.path || "";
+            options.path = PATH.join((options.base || ""), (options.path || ""));
+            
+            if (!options.path)
+                return callback(new Error("Invalid Path"));
+            
+            var args = assembleSearchCommand(options);
+            if (!args)
+                return callback(new Error("Invalid Arguments"));
+                
+            // if (this.activeProcess)
+            //     this.activeProcess.kill("SIGKILL");
+                
+            execute(args, function(err, results){
+                callback(err, results && results.stream);
+            });
+        }
+        
+        function execute(args, callback){
+            proc.spawn(NAK, {
+                args: args
+            }, function(err, process){
+                if (err)
+                    return callback(err);
+                
+                callback(null, { stream: process.stdout })
+            });
+        }
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("install", function(e){
+            install(e.next, e.progress);
+            return false;
+        });
+        
+        plugin.on("load", function(){
+            load();
+        });
+        
+        plugin.on("unload", function(){
+            loaded = false;
+        });
+        
+        /***** Register and define API *****/
+        
+        /**
+         * Finder implementation using nak
+         **/
+        plugin.freezePublicAPI({
+            /**
+             * Retrieves a list of files and lines that match a string or pattern
+             * @param options {Object}
+             *   object:
+             *   path           {String}  the path to search in (displayed in the results)
+             *   base           {String}  the base path to search in (is not displayed in the results)
+             *   query          {String}  the text or regexp to match the file contents with
+             *   casesensitive  {Boolean} whether to match on case or not. Default is false;
+             *   wholeword      {Boolean} whether to match the `query` as a whole word.
+             *   hidden         {String}  include files starting with a dott. Defaults to false.
+             *   regexp         {String}  whether the `query` is a regular expression.
+             *   pattern        {String}  specify what files/dirs to include 
+             *      and exclude. Prefix the words with minus '-' to exclude.
+             *   replaceAll     {Boolean} whether to replace the found matches
+             *   replacement    {String}  the string to replace the found matches with
+             * @param callback(err, results) {Function} called when the results come in
+             *   err     {Error}
+             *   results {String}
+             */
+            find : find,
+            
+            /**
+             * Retrieves a list of files under a path
+             * @param options {Object}
+             *   object:
+             *   path     {String}  the path to search in (displayed in the results)
+             *   base     {String}  the base path to search in (is not displayed in the results)
+             *   hidden   {Boolean} include files starting with a dott. Defaults to false.
+             *   maxdepth {Number}  maximum amount of parents a file can have.
+             * @param callback(err, results) {Function} called when the results come in
+             *   err     {Error}
+             *   results {Object}
+             *     data      {String}
+             *     count     {Number}
+             *     filecount {Number}
+             */
+            list : list
+        });
+        
+        register(null, {
+            finder: plugin
+        });
+    }
+});
