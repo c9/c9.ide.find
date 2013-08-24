b43600c396401750ae28c626d1d446b7d1a1138d
Ruben Daniels <ruben@c9.io>
2013-08-24 08:40:29 +0000

    Added vfs.client and auth to the tests

--- find.nak_test.js
+++ find.nak_test.js
@@ -37,7 +37,13 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
             ignore       : "file7_ignorable.rb",
             basePath     : baseProc
         },
-        "plugins/c9.fs/vfs",
+        {
+            packagePath: "plugins/c9.vfs.client/vfs_client",
+            smithIo     : {
+                "prefix": "/smith.io/server"
+            }
+        },
+        "plugins/c9.ide.auth/auth",
         "plugins/c9.fs/fs",
         "plugins/c9.fs/proc",
         
