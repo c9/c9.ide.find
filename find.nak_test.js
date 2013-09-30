bdc74b0daee0b18a2a7b5b4394531984b00c1419
Fabian Jakobs <fabian@c9.io>
2013-09-30 09:03:43 +0000

    don't pass along the smith.io config. Instead pass the list of vfs servers

--- find.nak_test.js
+++ find.nak_test.js
@@ -34,13 +34,8 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
             ignore       : "file7_ignorable.rb",
             basePath     : baseProc
         },
-        {
-            packagePath: "plugins/c9.vfs.client/vfs_client",
-            smithIo     : {
-                "path": "/smith.io/server"
-            }
-        },
-        "plugins/c9.vfs.client/endpoint.standalone",
+        "plugins/c9.vfs.client/vfs_client",
+        "plugins/c9.vfs.client/endpoint",
         "plugins/c9.ide.auth/auth",
         "plugins/c9.fs/fs",
         "plugins/c9.fs/proc",
