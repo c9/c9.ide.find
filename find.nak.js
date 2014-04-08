fb234600db4bbce957e66c8ac907a07032a2395d
Ruben Daniels <ruben@c9.io>
2014-04-08 17:20:21 +0200

    Fixes +2118. Don't write .nakignore in /.c9

--- find.nak.js
+++ find.nak.js
@@ -24,7 +24,7 @@ define(function(require, exports, module) {
         
         var USEHTTP     = options.useHttp;
         var IGNORE      = options.ignore;
-        var MAIN_IGNORE = "/.c9/.nakignore";
+        var MAIN_IGNORE = (options.local ? options.installPath : "") + "/.c9/.nakignore";
         var TEMPLATE    = require("text!./nakignore-template")
             + "\n" + (options.ignore || "");
         var NAK         = options.nak || "~/.c9/node_modules/nak/bin/nak";
